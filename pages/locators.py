6e58d9e4cbb5965c1f4e4ea2b1adcd8d78bb452f
Sergey Semenov <semenov.sergey.n@rt.ru>
2025-05-21 11:31:22 +0400

    Добавлены тесты корзины и базовые методы
    - Создан BasketPage с проверками:
      * should_be_empty()
      * should_not_be_products() (отрицательная проверка)
      * should_be_empty_message()
    
    - Добавлен метод go_to_basket_page() в BasePage
    - Реализованы новые тесты:
      * test_guest_cant_see_product_in_basket_opened_from_main_page
      * test_guest_cant_see_product_in_basket_opened_from_product_page
    
    - Добавлены соответствующие локаторы в BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -26,3 +26,8 @@ class ProductPageLocators:
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn-default")
+
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
